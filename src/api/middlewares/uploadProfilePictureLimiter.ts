5f848d3c5568a91608e372fc839c3f8ff2171e8a
jfelix61 <rtfire456@hotmail.com>
2019-07-01 14:20:49 -0400

    Changed message for handler to preserve error format

--- src/api/middlewares/uploadProfilePictureLimiter.ts
+++ src/api/middlewares/uploadProfilePictureLimiter.ts
@@ -3,7 +3,9 @@ import * as rateLimit from 'express-rate-limit';
 const uploadProfilePictureLimiter = new rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour window
   max: 5, // start blocking after 5 requests
-  message: 'Too many pictures uploaded, please try again after an hour',
+  handler: () => {
+    throw new Error('Too many pictures uploaded, please try again after an hour');
+  },
 });
 
 export default uploadProfilePictureLimiter;
