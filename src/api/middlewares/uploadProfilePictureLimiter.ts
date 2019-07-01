97563917e83d68947950690d3c39bef93da3148a
jfelix61 <rtfire456@hotmail.com>
2019-07-01 14:19:11 -0400

    Changed message of rate limit

--- src/api/middlewares/uploadProfilePictureLimiter.ts
+++ src/api/middlewares/uploadProfilePictureLimiter.ts
@@ -3,7 +3,7 @@ import * as rateLimit from 'express-rate-limit';
 const uploadProfilePictureLimiter = new rateLimit({
   windowMs: 60 * 60 * 1000, // 1 hour window
   max: 5, // start blocking after 5 requests
-  message: 'Too many accounts created from this IP, please try again after an hour',
+  message: 'Too many pictures uploaded, please try again after an hour',
 });
 
 export default uploadProfilePictureLimiter;
