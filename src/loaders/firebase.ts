ac0f1ea56daa421fa681757093db0d5adebc94c5
Jfelix61 <jfelix61@outlook.com>
2019-08-23 19:23:09 -0400

    removed comment from jobs

--- src/loaders/firebase.ts
+++ src/loaders/firebase.ts
@@ -2,6 +2,7 @@ import * as admin from 'firebase-admin';
 import config from '../config';
 
 const serviceAccount = config.firebase;
+console.log(serviceAccount);
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
--- src/loaders/jobs.ts
+++ src/loaders/jobs.ts
@@ -7,7 +7,6 @@ export default async ({ agenda }: { agenda: Agenda }) => {
   agenda.define(
     'send-email',
     { priority: 'high', concurrency: config.agenda.concurrency },
-    // @TODO Could this be a static method? Would it be better?
     new EmailSequenceJob().handler,
   );
 
