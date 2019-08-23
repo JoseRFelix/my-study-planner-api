7ef3c478062a8a2f8c5cbfa714fcd44c0c48e047
Jfelix61 <jfelix61@outlook.com>
2019-08-23 19:54:42 -0400

    Corrected production private key replace

--- src/loaders/firebase.ts
+++ src/loaders/firebase.ts
@@ -5,7 +5,7 @@ const serviceAccount = config.firebase;
 
 //Replace double backslash for single in production
 if (process.env.NODE_ENV === 'production')
-  serviceAccount.private_key = serviceAccount.private_key.replace('\\\\', '\\');
+  serviceAccount.private_key = serviceAccount.private_key.replace(/\\n/g, '\n');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount as any),
